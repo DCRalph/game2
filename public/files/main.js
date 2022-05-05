dfddfdd89bad428d8120b731bec36b11746737a6
DCRalph <43304937+DCRalph@users.noreply.github.com>
2022-05-05 14:02:07 +1200

    e

--- public/files/main.js
+++ public/files/main.js
@@ -1,12 +1,26 @@
 const nameBox = document.querySelector('#nameBox')
 const roomBox = document.querySelector('#roomBox')
-const gameBtns = document.querySelector('#games')
+const gameBox = document.querySelector('#gameBox')
+const joinGame = document.querySelector('#joinGame')
+const hostGame = document.querySelector('#hostGame')
+
+let games = []
+games.push('game 1')
+games.push('game 2')
+
+games.forEach((g) => {
+  let opt = document.createElement('option')
+  opt.innerText = g
+  console.log(gameBox)
+  gameBox.appendChild(opt)
+})
 
 const NewGame = async (type) => {
   const body = {
-    type: type,
+    gameType: type,
     name: nameBox.value,
     room: roomBox.value,
+    game: gameBox.value,
   }
 
   console.log(body)
@@ -22,48 +36,52 @@ const NewGame = async (type) => {
   const data = await res.json()
 
   console.log(data)
-
-  window.location.href = `/game/${data.gameId}`
 }
 
-const getData = async () => {
-  const res = await fetch('/userData')
-  const data = await res.json()
-  return data
-}
-
-const data = await getData()
-const games = data.games
-console.log(data)
-
-let btns = []
-
-for (const key in games) {
-  if (Object.hasOwnProperty.call(games, key)) {
-    const game = games[key]
-
-    const btn = document.createElement('button')
-    btn.innerText = game.name
-    btn.classList.add('gameBtn')
-    btn.setAttribute('disabled', '')
-    btn.addEventListener('click', () => {
-      NewGame(key)
-    })
-    gameBtns.appendChild(btn)
-    btns.push(btn)
+joinGame.addEventListener('click', () => {
+  if (nameBox.value.length > 0) {
+    NewGame('join')
   }
-}
-
-if (data.user.name) nameBox.value = data.user.name
-btns.forEach((b) => {
-  if (nameBox.value.length > 0) b.removeAttribute('disabled')
-  else b.setAttribute('disabled', '')
 })
 
-nameBox.addEventListener('keyup', () => {
-  let length = nameBox.value.length
-  btns.forEach((b) => {
-    if (length > 0) b.removeAttribute('disabled')
-    else b.setAttribute('disabled', '')
-  })
-})
+// const getData = async () => {
+//   const res = await fetch('/userData')
+//   const data = await res.json()
+//   return data
+// }
+
+// const data = await getData()
+// const games = data.games
+// console.log(data)
+
+// let btns = []
+
+// for (const key in games) {
+//   if (Object.hasOwnProperty.call(games, key)) {
+//     const game = games[key]
+
+//     const btn = document.createElement('button')
+//     btn.innerText = game.name
+//     btn.classList.add('gameBtn')
+//     btn.setAttribute('disabled', '')
+//     btn.addEventListener('click', () => {
+//       NewGame(key)
+//     })
+//     gameBtns.appendChild(btn)
+//     btns.push(btn)
+//   }
+// }
+
+// if (data.user.name) nameBox.value = data.user.name
+// btns.forEach((b) => {
+//   if (nameBox.value.length > 0) b.removeAttribute('disabled')
+//   else b.setAttribute('disabled', '')
+// })
+
+// nameBox.addEventListener('keyup', () => {
+//   let length = nameBox.value.length
+//   btns.forEach((b) => {
+//     if (length > 0) b.removeAttribute('disabled')
+//     else b.setAttribute('disabled', '')
+//   })
+// })
