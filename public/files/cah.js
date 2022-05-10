d0f9ea6fa9ebc4cbe50991792982151624e1d27f
DCRalph <43304937+DCRalph@users.noreply.github.com>
2022-05-11 00:12:19 +1200

    cum slut 19237918273

--- public/files/cah.js
+++ public/files/cah.js
@@ -147,7 +147,7 @@ const renderHand = () => {
     class="m-4 w-48 shrink-0 rounded-xl bg-white ring-4 ring-black relative select-none pointer-events-auto"
     id="white-card-${i}"
   >
-    <div class="text-2xl font-semibold px-4 py-2 pointer-events-none">${title}</div>
+    <div class="text-xl font-semibold px-4 py-2 pointer-events-none">${title}</div>
     ${makeSel(i)}
   </div>`
   }
