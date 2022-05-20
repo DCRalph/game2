a507322ae24a62e0bc7d34449dc435077eac20ce
DCRalph <43304937+DCRalph@users.noreply.github.com>
2022-05-20 16:57:27 +1200

    CUM

--- index.js
+++ index.js
@@ -233,7 +233,7 @@ app.post('/newgame', (req, res) => {
       // rooms[roomid].timer = undefined
       rooms[roomid] = undefined
       // delete rooms[roomid]
-    }, 1000 * 20)
+    }, 1000 * 60 * 5)
   }
 
   res.json({ ok: true, room: user.room })
